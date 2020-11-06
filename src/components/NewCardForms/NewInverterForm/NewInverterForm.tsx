2a2958a85a4325d28577c04cce8ab4267c065f21
Eduard <EduardTurdalijev@gmail.com>
2020-11-06 16:06:10 +0200

    Adding modal form fixes

--- src/components/NewCardForms/NewInverterForm/NewInverterForm.tsx
+++ src/components/NewCardForms/NewInverterForm/NewInverterForm.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useContext } from 'react';
-import { Form } from 'formik';
+import { Formik, Form } from 'formik';
 import { ModalContext } from '../../../context/ModalContext';
 import { useTranslation } from 'react-i18next';
 
@@ -8,18 +8,22 @@ const NewInverterForm: FunctionComponent = () => {
     const { closeModal } = useContext(ModalContext);
 
     return (
-        <Form className="form form-add-new">
-            <div className="left-column" />
-            <div className="right-column">
-                <div className="bottom-section">
-                    <div className="actions-toolbar">
-                        <button className="button-white" type="button" onClick={closeModal}>
-                            {t('Cancel')}
-                        </button>
+        <Formik initialValues={{}} onSubmit={() => {}}>
+            {() => (
+                <Form className="form form-add-new">
+                    <div className="left-column" />
+                    <div className="right-column">
+                        <div className="bottom-section">
+                            <div className="actions-toolbar">
+                                <button className="button-white" type="button" onClick={closeModal}>
+                                    {t('Cancel')}
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </Form>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
