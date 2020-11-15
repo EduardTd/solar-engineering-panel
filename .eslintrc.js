7615dea736682e0a413aa2c9b82602bc49f3729c
Eduard <EduardTurdalijev@gmail.com>
2020-11-15 16:24:13 +0200

    Prettier and ESLint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-        jest: true,
-    },
     extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'plugin:jsx-a11y/strict',
+        '@werkzeugkiste',
+        '@werkzeugkiste/eslint-config/react',
+        '@werkzeugkiste/eslint-config/typescript',
+        '@werkzeugkiste/eslint-config/node',
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
+    globals: {
+        __BROWSER__: true,
+        __SERVER__: true,
     },
-    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
-    rules: {
-        'no-var': 'error',
-        'brace-style': 'error',
-        'prefer-template': 'error',
-        radix: 'error',
-        'space-before-blocks': 'error',
-        'import/prefer-default-export': 'off',
-        'react/prop-types': 0,
-        'jsx-a11y/no-static-element-interactions': 'off',
-        'jsx-a11y/click-events-have-key-events': 'off',
-        'jsx-a11y/label-has-for': 'off',
-        'no-unused-vars': 'off',
-        'jsx-a11y/no-onchange': 'off',
-        'react-hooks/exhaustive-deps': 'off',
-        'no-debugger': 'off',
-    },
-    overrides: [
-        {
-            files: [
-                '**/*.test.js',
-                '**/*.test.jsx',
-                '**/*.test.tsx',
-                '**/*.spec.js',
-                '**/*.spec.jsx',
-                '**/*.spec.tsx',
-            ],
-            env: {
-                jest: true,
+    settings: {
+        'import/resolver': {
+            node: {
+                paths: ['src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
             },
         },
-    ],
+        'react': {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'import/no-unassigned-import': 0,
+        'import/no-named-as-default-member': 0,
+        'prettier/prettier': 'error',
+    },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,1 @@
-module.exports = {
-    printWidth: 100,
-    tabWidth: 4,
-    singleQuote: true,
-    semi: true,
-    trailingComma: 'all',
-    arrowParens: "always",
-    overrides: [
-        {
-            files: '*.{js,jsx,tsx,ts,scss,json,html}',
-            options: {
-                tabWidth: 4,
-            },
-        },
-    ],
-};
+module.exports = require('@werkzeugkiste/prettier-config');
